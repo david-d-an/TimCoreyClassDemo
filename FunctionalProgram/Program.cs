b0b8428d9fa05d5f3aab8dff381349e1a22fc109
David An <dong82@gmail.com>
2022-01-03 09:18:26 -0500

    Add Functional Program project

---
+++ FunctionalProgram/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace FunctionalProgram;
+
+class Program {
+    static void Main(string[] args) {
+        Disposable
+            .Using(
+                StreamFactory.GetStream,
+                stream => {
+                    var b = new byte[stream.Length];
+                    stream.Read(b, 0, b.Length);
+                    return b;
+                })
+            .Map(Encoding.UTF8.GetString)
+            .Split(new string[] { Environment.NewLine, " " }, StringSplitOptions.RemoveEmptyEntries)
+            .Select((s, i) => Tuple.Create(i, s))
+            .ToDictionary(t => t.Item1, t => t.Item2)
+            .Map(options => HtmlBuilder.BuildSelectBox("theDoctors", true));
+
+        // Transformation Pipeline
+        Disposable
+            .Using(
+                StreamFactory.GetStream,
+                stream => new byte[stream.Length].Tee(b => stream.Read(b, 0, (int)stream.Length)))
+            .Map(Encoding.UTF8.GetString)
+            .Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+            .Select((s, ix) => Tuple.Create(ix, s))
+            .ToDictionary(k => k.Item1, v => v.Item2)
+            .Map(HtmlBuilder.BuildSelectBox("theDoctors", true))
+            .Tee(Console.WriteLine);
+
+        string s1 = "null";
+        var r = Validator.IsNotNull(s1);
+
+        // String Validation Pipeline
+        "Doctor Who"
+            .Map(Validator.IsNotNull)
+            .Bind(Validator.IsNotEmpty)
+            .Bind(Validator.MinLength(8))
+            .Map(result => result.IsSuccess ? result.SuccessValue : result.FailureValue)
+            .Tee(Console.WriteLine);
+
+        Console.ReadLine();
+    }
+}
---
+++ FunctionalProgram/Result.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FunctionalProgram;
+
+public class Result<TSuccess, TFailure> {
+    // private readonly bool _isSuccess;
+
+    private Result(bool isSuccess){
+        // _isSuccess = isSuccess;
+        IsSuccess = isSuccess;
+    }
+
+    public TSuccess SuccessValue { get; private set; }
+    public TFailure FailureValue { get; private set; }
+    // public bool IsSuccess { get { return _isSuccess; } }
+    public bool IsSuccess { get; private init; }
+
+    public static Result<TSuccess, TFailure> SucceedWith(TSuccess value) =>
+        new Result<TSuccess, TFailure>(true) {
+            SuccessValue = value
+        };
+
+    public static Result<TSuccess, TFailure> FailWith(TFailure value) =>
+        new Result<TSuccess, TFailure>(false) {
+            FailureValue = value
+        };
+
+    public Result<TSuccess, TFailure> Bind(Func<TSuccess, Result<TSuccess, TFailure>> fn) =>
+        this.IsSuccess
+            ? fn(this.SuccessValue)
+            : this;
+}
+
+// public static class ResultHelp {
+//     public static Result<string, string> Bind2(this Result<string, string> @this, 
+//                                             Func<string, Result<string, string>> fn) =>
+//         @this.IsSuccess
+//             ? fn(@this.SuccessValue)
+//             : @this;
+// }
+
