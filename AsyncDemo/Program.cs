d593c061c936ff80b54a55dca80644160cd87e8e
david an <david.an@aireon.com>
2024-01-06 23:08:39 -0500

    Changed to top level statements

--- AsyncDemo/Program.cs
+++ AsyncDemo/Program.cs
@@ -2,36 +2,30 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace AsyncDemo {
-    class Program {
-        static async Task Main(string[] args) {
-            // await new Worker().RunAsync();
-            // await new ListOps().Run();
+// await new Worker().RunAsync();
+// await new ListOps().Run();
 
-            Task<int> downloading = DownloadDocsMainPageAsync();
-            // This is the only time downloading is evalueated by await.
-            await downloading;
+var downloading = DownloadDocsMainPageAsync();
+// This is the only time downloading is evalueated by await.
+await downloading;
 
-            // Subsequent await downloading will only reuse the previous eval.
-            int bytesLoaded = await downloading;
-            Console.WriteLine($"1. {nameof(Main)}: Downloaded {bytesLoaded} bytes.");
-            int bytesLoaded2 = await downloading;
-            Console.WriteLine($"2. {nameof(Main)}: Downloaded {bytesLoaded2} bytes.");
+// Subsequent await downloading will only reuse the previous eval.
+var bytesLoaded = await downloading;
+Console.WriteLine($"1. Main: Downloaded {bytesLoaded} bytes.");
+var bytesLoaded2 = await downloading;
+Console.WriteLine($"2. Main: Downloaded {bytesLoaded2} bytes.");
+return;
 
-            // ReturnAwaitTest.GetIFooAsyncNoWait doesn't work due to type incompatibility
-            // IFoo foo = await ReturnAwaitTest.GetIFooAsync();
-            // Console.WriteLine($"foo.id = {foo.id}     foo.timestamp = {foo.timestamp}");
-        }
+// ReturnAwaitTest.GetIFooAsyncNoWait doesn't work due to type incompatibility
+// IFoo foo = await ReturnAwaitTest.GetIFooAsync();
+// Console.WriteLine($"foo.id = {foo.id}     foo.timestamp = {foo.timestamp}");
 
+static async Task<int> DownloadDocsMainPageAsync() {
+    Console.WriteLine($"{nameof(DownloadDocsMainPageAsync)}: About to start downloading.");
 
-        private static async Task<int> DownloadDocsMainPageAsync() {
-            Console.WriteLine($"{nameof(DownloadDocsMainPageAsync)}: About to start downloading.");
+    var client = new HttpClient();
+    var content = await client.GetByteArrayAsync("https://docs.microsoft.com/en-us/");
 
-            var client = new HttpClient();
-            byte[] content = await client.GetByteArrayAsync("https://docs.microsoft.com/en-us/");
-
-            Console.WriteLine($"{nameof(DownloadDocsMainPageAsync)}: Finished downloading.");
-            return content.Length;
-        }
-    }
+    Console.WriteLine($"{nameof(DownloadDocsMainPageAsync)}: Finished downloading.");
+    return content.Length;
 }
