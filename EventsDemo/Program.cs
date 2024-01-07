a5b816fc8a57b259a11abe1389a89275f216775f
david an <david.an@aireon.com>
2024-01-07 14:05:03 -0500

    Changed to top level

---
+++ EventsDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿using EventsDemo;
+
+var process = new SignalProcessor();
+var signalHandler = new SignalHandler();
+process.SignalHandler += signalHandler.OnSignal;
+
+process.LoopSignal();
+/* This works only if SignalHandler is a non-event delegate. */
+/* This is a syntax error if SignalHandler is an event. */
+// process.SignalHandler(null, null);
