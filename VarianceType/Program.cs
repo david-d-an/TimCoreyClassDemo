02069b70422564ab6e9bc4eccb3edbee086916b7
david an <david.an@aireon.com>
2024-01-07 15:23:55 -0500

    Upgrded to net8 and converted to top level

--- VarianceType/Program.cs
+++ VarianceType/Program.cs
@@ -1,53 +1,34 @@
-﻿using System;
-
-namespace VarianceType
-{
-    class Program
-    {
-        public class Control {}
-        public class Button : Control {}
-
-        // in: Contra
-        // out: Co
-
-        // Contravariant delegate.
-        public delegate void DContravariant<in T>(T argument);
-        public delegate T DCovariant<out T>();
-
-        // Methods that match the delegate signature.
-        public static void ContravariantControl(Control argument)
-        { }
-        public static void ContravariantButton(Button argument)
-        { }
-
-        public static Control CovariantControl()
-        { return new Control(); }
-        public static Button CovariantButton()
-        { return new Button(); }
+﻿using VarianceType.Models;
 
 
-        static void Main(string[] args)
-        {
-            Func<int, int, double> foo = (a, b) => a + b + GetVal();
-            Console.WriteLine($"Hello World: {foo(2, 3)}");
+// Contravariant
+// DContravariant<Control> dContraControl = ContravariantControl;
+// DContravariant<Button> dContraButton = ContravariantButton;
+// dContraButton = dContraControl;
+DContravariant<Button> dContraButton = ContravariantControl;
+dContraButton(new Button());
 
-            // Contravariant
-            // DContravariant<Control> dContraControl = ContravariantControl;
-            // DContravariant<Button> dContraButton = ContravariantButton;
-            // dContraButton = dContraControl;
-            DContravariant<Button> dContraButton = ContravariantControl;
-            dContraButton(new Button());
+// Covariant
+// DCovariant<Control> dCoControl = CovariantControl;
+// DCovariant<Button> dCoButton = CovariantButton;
+// dCoControl = dCoButton;
+DCovariant<Control> dCoControl = CovariantButton;
+Control c = dCoControl();
+return;
 
-            // Covariant
-            // DCovariant<Control> dCoControl = CovariantControl;
-            // DCovariant<Button> dCoButton = CovariantButton;
-            // dCoControl = dCoButton;
-            DCovariant<Control> dCoControl = CovariantButton;
-            Control c = dCoControl();
-        }
+// in: Contra
+// out: Co
 
-        private static int GetVal() {
-            return new Random().Next();
-        }
-    }
-}
+// Methods that match the delegate signature.
+void ContravariantControl(Control argument)
+{ }
+void ContravariantButton(Button argument)
+{ }
+
+Control CovariantControl() => new Control();
+
+Button CovariantButton() => new Button();
+
+// Contravariant delegate.
+public delegate void DContravariant<in T>(T argument);
+public delegate T DCovariant<out T>();
