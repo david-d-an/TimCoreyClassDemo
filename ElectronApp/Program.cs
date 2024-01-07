9053e94d3a6bbf7ea110f845c228a018d38106a6
david an <david.an@aireon.com>
2024-01-07 14:33:17 -0500

    Cleaned up

--- ElectronApp/Program.cs
+++ ElectronApp/Program.cs
@@ -1,23 +1,17 @@
+using ElectronApp;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using ElectronNET.API;
 
-namespace ElectronApp {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+CreateHostBuilder(args).Build().Run();
+return;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    // Use Electron APIs
-                    webBuilder.UseElectron(args);
-                    webBuilder.UseEnvironment("Development");
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+static IHostBuilder CreateHostBuilder(string[] args) => Host
+    .CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder =>
+    {
+        // Use Electron APIs
+        webBuilder.UseElectron(args);
+        webBuilder.UseEnvironment("Development");
+        webBuilder.UseStartup<Startup>();
+    });
--- ElectronApp/Startup.cs
+++ ElectronApp/Startup.cs
@@ -5,51 +5,51 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ElectronNET.API;
 
-namespace ElectronApp {
-    public class Startup {
-        public Startup(IConfiguration configuration) {
-            Configuration = configuration;
+namespace ElectronApp;
+
+public class Startup {
+    public IConfiguration Configuration { get; init; }
+
+    public Startup(IConfiguration configuration) {
+        Configuration = configuration;
+    }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services) {
+        services.AddRazorPages();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+        if (env.IsDevelopment()) {
+            app.UseDeveloperExceptionPage();
+        }
+        else {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
 
-        public IConfiguration Configuration { get; }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
-            services.AddRazorPages();
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints => {
+            endpoints.MapRazorPages();
+        });
+
+        if (HybridSupport.IsElectronActive) {
+            CreateWindow();
         }
+    }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment()) {
-                app.UseDeveloperExceptionPage();
-            }
-            else {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints => {
-                endpoints.MapRazorPages();
-            });
-
-            if (HybridSupport.IsElectronActive) {
-                CreateWindow();
-            }
-        }
-
-        private async void CreateWindow() {
-            var window = await Electron.WindowManager.CreateWindowAsync();
-            window.OnClosed += () => {
-                Electron.App.Quit();
-            };
-        }
+    private static async void CreateWindow() {
+        var window = await Electron.WindowManager.CreateWindowAsync();
+        window.OnClosed += () => {
+            Electron.App.Quit();
+        };
     }
 }
