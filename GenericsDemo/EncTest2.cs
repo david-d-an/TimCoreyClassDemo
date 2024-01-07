4001d71090e52394121e48ef06ae8d02959b6d04
david an <david.an@aireon.com>
2024-01-06 19:56:29 -0500

    GenericsDemo updated.

---
+++ GenericsDemo/EncTest2.cs
@@ -0,0 +1,74 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace GenericsDemo;
+
+public class EncTest2
+{
+    private const int KeySize = 128;
+    private const int BlockSize = 128;
+    public static string? Key { get; set; }
+
+    public static byte[]? KeyByte => Key == null ? null : ConvertHexStringToByteArray(Key);
+
+    public static byte[] EncryptByteToByte(string plainText) =>
+        EncryptByteToByte(ConvertHexStringToByteArray(plainText));
+
+    public static byte[] EncryptByteToByte(byte[] plainTextByte)
+    {
+        if (KeyByte == null || KeyByte.Length == 0)
+            throw new InvalidDataException("Key bytes are missing");
+
+        using var aes = BuildAes(KeyByte);
+        ICryptoTransform encryptor = aes.CreateEncryptor();
+
+        byte[] encryptedBytes = encryptor.TransformFinalBlock(plainTextByte, 0, plainTextByte.Length);
+        return encryptedBytes;
+    }
+
+    public static byte[] DecryptByteToByte(string cipher) =>
+        DecryptByteToByte(ConvertHexStringToByteArray(cipher));
+
+    public static byte[] DecryptByteToByte(byte[] cipherByte)
+    {
+        if (KeyByte == null || KeyByte.Length == 0)
+            throw new InvalidDataException("Key bytes are missing");
+
+        using var aes = BuildAes(KeyByte);
+        ICryptoTransform transform = aes.CreateDecryptor();
+
+        byte[] decryptedBytes = transform.TransformFinalBlock(cipherByte, 0, cipherByte.Length);
+        return decryptedBytes;
+    }
+
+    private static Aes BuildAes(byte[] key)
+    {
+        var aes = Aes.Create();
+        aes.Mode = CipherMode.ECB;
+        aes.Padding = PaddingMode.None;
+        aes.KeySize = KeySize;
+        aes.BlockSize = BlockSize;
+        aes.Key = key;
+        return aes;
+    }
+
+    private static byte[] ConvertHexStringToByteArray(string hexString)
+    {
+        if (hexString.Length % 2 != 0)
+            throw new ArgumentException(
+                "Input string must have an even number of characters.");
+
+        byte[] byteArray = new byte[hexString.Length / 2];
+
+        for (int i = 0; i < hexString.Length; i += 2)
+        {
+            string hexByte = hexString.Substring(i, 2);
+            byteArray[i / 2] = Convert.ToByte(hexByte, 16);
+        }
+
+        return byteArray;
+    }
+ }
---
+++ GenericsDemo/EncTestBouncyCastle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+namespace GenericsDemo;
+
+public class EncTestBouncyCastle
+{
+    public static byte[] EncryptAes(string input, string key)
+    {
+        byte[] plaintext = StringToByteArray(input);  // Encoding.UTF8.GetBytes(input);
+        byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+
+        // Create AES cipher with ECB mode and PKCS7 padding
+        // NOPADDING
+        IBufferedCipher cipher = CipherUtilities.GetCipher("AES/ECB/NOPADDING");
+        cipher.Init(true, new KeyParameter(keyBytes));
+
+        // Encrypt the plaintext
+        byte[] ciphertext = cipher.DoFinal(plaintext);
+        return ciphertext;
+    }
+
+    public static string DecryptAes(byte[] ciphertext, string key)
+    {
+        byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+
+        // Create AES cipher with ECB mode and PKCS7 padding
+        IBufferedCipher cipher = CipherUtilities.GetCipher("AES/ECB/NOPADDING");
+        cipher.Init(false, new KeyParameter(keyBytes));
+
+        // Decrypt the ciphertext
+        byte[] decryptedBytes = cipher.DoFinal(ciphertext);
+
+        // Convert the decrypted bytes to a string
+        // string decryptedText = Encoding.UTF8.GetString(decryptedBytes);
+        // return decryptedText;
+        return GetHexString(decryptedBytes);
+    }
+    
+    public static byte[] StringToByteArray(string hex) =>
+        Enumerable.Range(0, hex.Length)
+            .Where(x => x % 2 == 0)
+            .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+            .ToArray();
+    
+    public static string GetHexString(byte[] byteArray) =>
+        BitConverter.ToString(byteArray).Replace("-", "");
+}
