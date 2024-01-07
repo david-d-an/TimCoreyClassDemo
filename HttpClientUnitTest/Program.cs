a0305e8b679e5539a999fce98e5065a998c2dff9
david an <david.an@aireon.com>
2024-01-07 14:38:25 -0500

    Upgraded to .net8 and cleaned up

--- HttpClientUnitTest/Program.cs
+++ HttpClientUnitTest/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,51 +6,45 @@ using Moq;
 using Moq.Protected;
 using Xunit;
 
-namespace HttpClientUnitTest {
-    // class Program {
-    //     public static void Main(string[] args) {
-    //         var analyzer = new SiteAnalyzer(Client);
-    //         var size = analyzer.GetContentSize("http://microsoft.com").Result;
-    //         Console.WriteLine($"Size: {size}");
-    //     }
-    //
-    //     private static readonly HttpClient Client = new HttpClient(); // Singleton
-    // }
+namespace HttpClientUnitTest;
+// class Program {
+//     public static void Main(string[] args) {
+//         var analyzer = new SiteAnalyzer(Client);
+//         var size = analyzer.GetContentSize("http://microsoft.com").Result;
+//         Console.WriteLine($"Size: {size}");
+//     }
+//
+//     private static readonly HttpClient Client = new HttpClient(); // Singleton
+// }
 
-    public class SiteAnalyzer {
-        public SiteAnalyzer(HttpClient httpClient) {
-            _httpClient = httpClient;
-        }
-
-        public async Task<int> GetContentSize(string uri)
-        {
-            var response = await _httpClient.GetAsync( uri );
-            var content = await response.Content.ReadAsStringAsync();
-            return content.Length;
-        }
-
-        private readonly HttpClient _httpClient;
+public class SiteAnalyzer(HttpClient httpClient)
+{
+    public async Task<int> GetContentSize(string uri)
+    {
+        var response = await httpClient.GetAsync( uri );
+        var content = await response.Content.ReadAsStringAsync();
+        return content.Length;
     }
+}
 
-    public class SiteAnalyzerTests {
-        [Fact]
-        public async void GetContentSizeReturnsCorrectLength() {
-            // Arrange
-            const string testContent = "test content";
-            var mockMessageHandler = new Mock<HttpMessageHandler>();
-            mockMessageHandler.Protected()
-                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
-                .ReturnsAsync(new HttpResponseMessage {
-                    StatusCode = HttpStatusCode.OK,
-                    Content = new StringContent(testContent)
-                });
-            var underTest = new SiteAnalyzer(new HttpClient(mockMessageHandler.Object));
+public class SiteAnalyzerTests {
+    [Fact]
+    public async void GetContentSizeReturnsCorrectLength() {
+        // Arrange
+        const string testContent = "test content";
+        var mockMessageHandler = new Mock<HttpMessageHandler>();
+        mockMessageHandler.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(testContent)
+            });
+        var underTest = new SiteAnalyzer(new HttpClient(mockMessageHandler.Object));
 
-            // Act
-            var result = await underTest.GetContentSize("http://anyurl");
+        // Act
+        var result = await underTest.GetContentSize("http://anyurl");
 
-            // Assert
-            Assert.Equal(testContent.Length, result);
-        }
+        // Assert
+        Assert.Equal(testContent.Length, result);
     }
-}    
+}
