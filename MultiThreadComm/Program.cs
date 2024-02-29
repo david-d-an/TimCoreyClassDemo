ddaa4d6e0d2162df6563c2675eb1565eb81a70b9
david an <david.an@aireon.com>
2024-02-29 10:49:41 -0500

    feat: Add Program.cs

---
+++ MultiThreadComm/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Concurrent;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MultiThreadComm;
+
+var messageQueue = new BlockingCollection<string>(boundedCapacity: 10);
+
+var serviceCollection = new ServiceCollection();
+var serviceProvider = serviceCollection
+    .AddLogging(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Information))
+    .BuildServiceProvider();
+
+new Worker(serviceProvider, messageQueue).Start();
